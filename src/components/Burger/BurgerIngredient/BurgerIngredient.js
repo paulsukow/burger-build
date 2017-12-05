736668a410869f6e8024bdd1a11a4995fba62497
psukow <psukow@unifocus.com>
2017-12-05 14:15:49 -0700

    started the burger component

--- src/components/Burger/BurgerIngredient/BurgerIngredient.js
+++ src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -1,6 +1,6 @@
-import React { Component } from 'react';
+import React, { Component } from 'react';
 
-import classes from './burgerIngredient.css';
+import classes from './BurgerIngredient.css';
 import PropTypes from 'prop-types';
 
 class BurgerIngredient extends Component {
@@ -9,7 +9,7 @@ class BurgerIngredient extends Component {
 
     switch (this.props.type) {
       case ('bread-bottom'):
-        ingredient = <div> className={classes.BreadBottom}></div>;
+        ingredient = <div className={classes.BreadBottom}></div>;
         break;
       case ('bread-top'):
         ingredient = (
@@ -20,19 +20,19 @@ class BurgerIngredient extends Component {
         );
         break;
       case ('meat'):
-        ingredient = <div> className={classes.Meat}></div>;
+        ingredient = <div className={classes.Meat}></div>;
         break;
 
       case ('cheese'):
-        ingredient = <div> className={classes.Cheese}></div>;
+        ingredient = <div className={classes.Cheese}></div>;
         break;
 
       case ('salad'):
-        ingredient = <div> className={classes.Salad}></div>;
+        ingredient = <div className={classes.Salad}></div>;
         break;
 
       case ('bacon'):
-        ingredient = <div> className={classes.Bacon}></div>;
+        ingredient = <div className={classes.Bacon}></div>;
         break;
       default:
         ingredient = null;

09dee34497cc5275935d482f1d20ce3dc9237c51
Paul Sukow <paul.sukow@gmail.com>
2018-04-14 18:56:55 +0800

    added logic to handle user input to form

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -10,20 +10,23 @@ const input = (props) => {
             inputElement = <input 
                 className={classes.InputElement} 
                 {...props.elementConfig} 
-                value={props.value}/>;
+                value={props.value}
+                onChange={props.changed}/>;
             break;
         case ( 'textarea' ):
             inputElement = <textarea 
                 className={classes.InputElement} 
                 {...props.elementConfig} 
-                value={props.value}/>;        
+                value={props.value}
+                onChange={props.changed}/>;        
             break;
         case ( 'select'):
             inputElement = (
                 <select
                     className={classes.InputElement} 
                     {...props.elementConfig} 
-                    value={props.value}>
+                    value={props.value}
+                    onChange={props.changed}>
                     {props.elementConfig.options.map(option => (
                         <option key={option.value} value={option.value}>
                             {option.displayValue}
@@ -36,7 +39,8 @@ const input = (props) => {
             inputElement = <input
                 className={classes.InputElement} 
                 {...props.elementConfig} 
-                value={props.value}/>;           
+                value={props.value}
+                onChange={props.changed}/>;           
     }
 
     return (

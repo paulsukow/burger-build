b5e182bab97cc6ff1d36aa551794eecf27815bd9
Paul Sukow <paul.sukow@gmail.com>
2018-04-18 17:11:46 +0800

    added disabled the button when the form is not valid

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -89,10 +89,12 @@ class ContactData extends Component {
                         {value: 'cheapest', displayValue: 'Cheapest'}
                     ]
                 },
-                value: ''
+                value: '',
+                valid: true
             },
         },
-        loading: false
+        loading: false,
+        formIsValid: false
     }
 
     orderHandler = (event) => {
@@ -149,7 +151,13 @@ class ContactData extends Component {
         updatedFormElement.valid = this.checkValidity(updatedFormElement.value, updatedFormElement.validation)
         updatedFormElement.touched = true;
         updatedOrderForm[inputIdentifier] = updatedFormElement;
-        this.setState({orderForm: updatedOrderForm});
+
+        let formIsValid = true;
+        for (let inputIdentifier in updatedOrderForm) {
+            formIsValid = updatedOrderForm[inputIdentifier].valid && formIsValid;
+        }
+
+        this.setState({orderForm: updatedOrderForm, formIsValid: formIsValid});
     }
 
     render () {
@@ -174,7 +182,10 @@ class ContactData extends Component {
                         valueType={formElement.config.valueType}
                         changed={(event) => this.inputChangedHandler(event, formElement.id)}/>
                 ))}
-                <Button btnType='Success'>ORDER</Button>
+                <Button 
+                    btnType='Success' 
+                    disabled={!this.state.formIsValid}>ORDER
+                </Button>
             </form>
         );
         if (this.state.loading) {

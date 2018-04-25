c950c180c626697aae2b43c77383ad45e662fce7
Paul Sukow <paul.sukow@gmail.com>
2018-04-25 20:09:36 +0800

    transfered ingredient state from burgerbuilder.js to redux

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,12 +1,35 @@
 import * as actionTypes from './actions';
 
 const initialState = {
-    ingredients: null,
-    totalPrice: 4
+    ingredients: {
+        lettuce: 0,
+        bacon: 0,
+        cheese: 0,
+        meat: 0
+    }
 }
 
 const reducer = (state = initialState, action) => {
-
+    switch (action.type) {
+        case actionTypes.ADD_INGREDIENT:
+            return {
+                ...state,
+                ingredients: {
+                    ...state.ingredients,
+                    [action.ingredientName]: state.ingredients[action.ingredientName] + 1
+                }
+            };
+        case actionTypes.REMOVE_INGREDIENT:
+            return {
+                ...state,
+                ingredients: {
+                    ...state.ingredients,
+                    [action.ingredientName]: state.ingredients[action.ingredientName] - 1
+                }
+            };
+        default: 
+            return state;
+    }
 };
 
 export default reducer;

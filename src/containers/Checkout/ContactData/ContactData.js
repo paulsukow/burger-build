a2a9987f51121b7101ab2214ea382fb89416d78e
Paul Sukow <paul.sukow@gmail.com>
2018-04-26 17:14:48 +0800

    modifed Checkout and ContactData to use Redux for state managment

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 import Spinner from '../../../components/UI/Spinner/Spinner';
 import Button from '../../../components/UI/Button/Button';
@@ -109,7 +110,7 @@ class ContactData extends Component {
         }
 
         const order = {
-            ingredients: this.props.ingredients,
+            ingredients: this.props.ings,
             price: this.props.price,
             orderDate: formData
         }
@@ -203,4 +204,11 @@ class ContactData extends Component {
     }
 } 
 
-export default ContactData;
+const mapStateToProps = state => {
+    return {
+        ings: state.ingredients,
+        price: state.totalPrice
+    }
+};
+
+export default connect(mapStateToProps)(ContactData);
